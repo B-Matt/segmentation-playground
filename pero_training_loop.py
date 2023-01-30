c00aef0034c05274e2fa965cd17b0e969123c31a
Petar Maric <petarmari266@gmail.com>
2023-01-30 13:49:53 +0100

    Add FPN to training loop

--- pero_training_loop.py
+++ pero_training_loop.py
@@ -1,18 +1,26 @@
+import datetime
 import subprocess
-from datetime import datetime
 
 training_setups = [
-    r'./.venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="efficientnet-b4" --epochs=200 --batch-size=144 --patch-size=256 --weight-decay=1e-4 --save-checkpoints',
-    r'./.venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="efficientnet-b7" --epochs=200 --batch-size=80 --patch-size=256 --weight-decay=1e-4 --save-checkpoints',
-    r'./.venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="resnext50_32x4d" --epochs=200 --batch-size=70 --patch-size=256 --weight-decay=1e-4 --save-checkpoints',
+    r'./venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="efficientnet-b4" --epochs=200 --batch-size=144 --patch-size=256 --weight-decay=1e-4 --save-checkpoints',
+    r'./venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="efficientnet-b7" --epochs=200 --batch-size=80 --patch-size=256 --weight-decay=1e-4 --save-checkpoints',
+    r'./venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="resnext50_32x4d" --epochs=200 --batch-size=70 --patch-size=256 --weight-decay=1e-4 --save-checkpoints',
+    r'./venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="efficientnet-b4" --epochs=200 --batch-size=22 --patch-size=640 --weight-decay=1e-4 --save-checkpoints',
+    r'./venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="efficientnet-b7" --epochs=200 --batch-size=12 --patch-size=640 --weight-decay=1e-4 --save-checkpoints',
+    r'./venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="resnext50_32x4d" --epochs=200 --batch-size=10 --patch-size=640 --weight-decay=1e-4 --save-checkpoints',
+    r'./venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="efficientnet-b4" --epochs=200 --batch-size=10 --patch-size=800 --weight-decay=1e-4 --save-checkpoints',
+    r'./venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="efficientnet-b7" --epochs=200 --batch-size=6 --patch-size=800 --weight-decay=1e-4 --save-checkpoints',
+    r'./venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="resnext50_32x4d" --epochs=200 --batch-size=10 --patch-size=800 --weight-decay=1e-4 --save-checkpoints',
 
-    r'./.venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="efficientnet-b4" --epochs=200 --batch-size=22 --patch-size=640 --weight-decay=1e-4 --save-checkpoints',
-    r'./.venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="efficientnet-b7" --epochs=200 --batch-size=12 --patch-size=640 --weight-decay=1e-4 --save-checkpoints',
-    r'./.venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="resnext50_32x4d" --epochs=200 --batch-size=10 --patch-size=640 --weight-decay=1e-4 --save-checkpoints',
-
-    r'./.venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="efficientnet-b4" --epochs=200 --batch-size=10 --patch-size=800 --weight-decay=1e-4 --save-checkpoints',
-    r'./.venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="efficientnet-b7" --epochs=200 --batch-size=6 --patch-size=800 --weight-decay=1e-4 --save-checkpoints',
-    r'./.venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="resnext50_32x4d" --epochs=200 --batch-size=10 --patch-size=800 --weight-decay=1e-4 --save-checkpoints',
+    r'./.venv/Scripts/python train.py --model="FPN" --encoder="efficientnet-b4" --epochs=200 --batch-size=155 --patch-size=256 --weight-decay=1e-4 --save-checkpoints',
+    r'./.venv/Scripts/python train.py --model="FPN" --encoder="efficientnet-b7" --epochs=200 --batch-size=95 --patch-size=256 --weight-decay=1e-4 --save-checkpoints',
+    r'./.venv/Scripts/python train.py --model="FPN" --encoder="resnext50_32x4d" --epochs=200 --batch-size=316 --patch-size=256 --weight-decay=1e-4 --save-checkpoints',
+    r'./.venv/Scripts/python train.py --model="FPN" --encoder="efficientnet-b4" --epochs=200 --batch-size=25 --patch-size=640 --weight-decay=1e-4 --save-checkpoints',
+    r'./.venv/Scripts/python train.py --model="FPN" --encoder="efficientnet-b7" --epochs=200 --batch-size=12 --patch-size=640 --weight-decay=1e-4 --save-checkpoints',
+    r'./.venv/Scripts/python train.py --model="FPN" --encoder="resnext50_32x4d" --epochs=200 --batch-size=40 --patch-size=640 --weight-decay=1e-4 --save-checkpoints',
+    r'./.venv/Scripts/python train.py --model="FPN" --encoder="efficientnet-b4" --epochs=200 --batch-size=12 --patch-size=800 --weight-decay=1e-4 --save-checkpoints',
+    r'./.venv/Scripts/python train.py --model="FPN" --encoder="efficientnet-b7" --epochs=200 --batch-size=10 --patch-size=800 --weight-decay=1e-4 --save-checkpoints',
+    r'./.venv/Scripts/python train.py --model="FPN" --encoder="resnext50_32x4d" --epochs=200 --batch-size=10 --patch-size=800 --weight-decay=1e-4 --save-checkpoints',
 ]
 
 for training in training_setups:
