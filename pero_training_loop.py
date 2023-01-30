d2e359f524e2f648ba2235ffb31f5931afcfe28e
Petar Maric <petarmari266@gmail.com>
2023-01-30 13:45:03 +0100

    Add custom training loop; add .idea to gitignore

---
+++ pero_training_loop.py
@@ -0,0 +1,23 @@
+import subprocess
+from datetime import datetime
+
+training_setups = [
+    r'./.venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="efficientnet-b4" --epochs=200 --batch-size=144 --patch-size=256 --weight-decay=1e-4 --save-checkpoints',
+    r'./.venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="efficientnet-b7" --epochs=200 --batch-size=80 --patch-size=256 --weight-decay=1e-4 --save-checkpoints',
+    r'./.venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="resnext50_32x4d" --epochs=200 --batch-size=70 --patch-size=256 --weight-decay=1e-4 --save-checkpoints',
+
+    r'./.venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="efficientnet-b4" --epochs=200 --batch-size=22 --patch-size=640 --weight-decay=1e-4 --save-checkpoints',
+    r'./.venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="efficientnet-b7" --epochs=200 --batch-size=12 --patch-size=640 --weight-decay=1e-4 --save-checkpoints',
+    r'./.venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="resnext50_32x4d" --epochs=200 --batch-size=10 --patch-size=640 --weight-decay=1e-4 --save-checkpoints',
+
+    r'./.venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="efficientnet-b4" --epochs=200 --batch-size=10 --patch-size=800 --weight-decay=1e-4 --save-checkpoints',
+    r'./.venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="efficientnet-b7" --epochs=200 --batch-size=6 --patch-size=800 --weight-decay=1e-4 --save-checkpoints',
+    r'./.venv/Scripts/python train.py --model="UnetPlusPlus" --encoder="resnext50_32x4d" --epochs=200 --batch-size=10 --patch-size=800 --weight-decay=1e-4 --save-checkpoints',
+]
+
+for training in training_setups:
+    start_time = datetime.datetime.now()
+    subprocess.call(training)
+    end_time = datetime.datetime.now()
+
+    print(f"Training run took: {end_time - start_time}.")
