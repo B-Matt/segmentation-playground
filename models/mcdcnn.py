7b67310c5d5927520624f79c5cf9634e8db29ea1
Matej Arlović <matej.arlovic@gmail.com>
2023-03-07 15:11:41 +0100

    MCDCNN made faster where conv 1x1 changed with conv 3x3.

--- models/mcdcnn.py
+++ models/mcdcnn.py
@@ -2,52 +2,67 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class ConvLayer(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-        )
-
-    def forward(self, x):
-        return self.conv(x)
-
 class MCDCNN(nn.Module):
-    def __init__(self, dropout = 0.3, input_channels = 4, output_channel = 1):
+    def __init__(self, dropout = 0.3, input_channels = 4, output_channel = 1, num_layers = 6):
         super(MCDCNN, self).__init__()
 	
-        # _input_channels_list = [input_channels, 32, 64, 128, 256, 512]
-        # _output_channels_list = [32, 64, 128, 256, 512, 1024] 
+        self.conv_layers = nn.ModuleList()
+        _input_channels_list = [input_channels, 8, 16, 32, 64, 128]
+        _output_channels_list = [8, 16, 32, 64, 128, 256]
 
-        # _input_channels_list = [input_channels, 32, 64, 128, 256, 512]
-        # _output_channels_list = [32, 64, 128, 256, 512, 1024]
+        for i in range(num_layers):
+            self.conv_layers.append(nn.Conv2d(_input_channels_list[i], _output_channels_list[i], kernel_size=3, padding=1, bias=True))
+            self.conv_layers.append(nn.BatchNorm2d(num_features=_output_channels_list[i]))
+            self.conv_layers.append(nn.ReLU(inplace=True))
+            self.conv_layers.append(nn.Dropout2d(p=dropout))
 
-        _input_channels_list = [input_channels, 16, 32, 64, 128, 256, 512]
-        _output_channels_list = [16, 32, 64, 128, 256, 512, 1024]
-
-        self.conv1 = ConvLayer(_input_channels_list[0], _output_channels_list[0])
-        self.conv2 = ConvLayer(_input_channels_list[1], _output_channels_list[1])
-        self.conv3 = ConvLayer(_input_channels_list[2], _output_channels_list[2])
-        self.conv4 = ConvLayer(_input_channels_list[3], _output_channels_list[3])
-        self.conv5 = ConvLayer(_input_channels_list[4], _output_channels_list[4])
-        self.conv6 = ConvLayer(_input_channels_list[5], _output_channels_list[5])
-        self.conv7 = ConvLayer(_input_channels_list[6], _output_channels_list[6])
-
-        self.final_conv = nn.Conv2d(1024, output_channel, kernel_size=1)
-        self.dropout = nn.Dropout2d(p=dropout)
+        self.final_conv = nn.Conv2d(256, output_channel, kernel_size=1)
     
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
-        x = self.conv6(x)
-        x = self.conv7(x)
+        for layer in self.conv_layers:
+            x = layer(x)
 
         x = self.final_conv(x)
-        x = self.dropout(x)
+        return torch.sigmoid(x)
+
+class MCDCNN_New(nn.Module):
+    def __init__(self, dropout = 0.3, input_channels = 4, output_channel = 1, num_layers = 6):
+        super(MCDCNN_New, self).__init__()
+	
+        self.num_layers = num_layers
+        self.conv_layers = nn.ModuleList()
+	
+        _input_channels_list = [input_channels, 8, 16, 32, 64, 128]
+        _output_channels_list = [8, 16, 32, 64, 128, 256]
+
+        for i in range(num_layers):
+            self.conv_layers.append(nn.Conv2d(_input_channels_list[i], _output_channels_list[i], kernel_size=3, padding=1, bias=True))
+            # self.conv_layers.append(nn.BatchNorm2d(num_features=_output_channels_list[i]))
+            self.conv_layers.append(nn.ReLU(inplace=True))
+            self.conv_layers.append(nn.Dropout2d(p=dropout))
+
+        self.max_pool = nn.MaxPool2d(kernel_size=2, stride=1)
+        self.final_conv = nn.Conv2d(1024, output_channel, kernel_size=1)
+    
+    def forward(self, img1, img2, img3, img4):
+        ch1_out = img1
+        for layer in self.conv_layers:
+            ch1_out = layer(ch1_out)
+
+        ch2_out = img2
+        for layer in self.conv_layers:
+            ch2_out = layer(ch2_out)
+
+        ch3_out = img3
+        for layer in self.conv_layers:
+            ch3_out = layer(ch3_out)
+
+        ch4_out = img4
+        for layer in self.conv_layers:
+            ch4_out = layer(ch4_out)
+
+        x = torch.cat([ch1_out, ch2_out, ch3_out, ch4_out], dim=1)
+        # x = self.max_pool(x)
+        # print('eeee', x.shape)
+        x = self.final_conv(x)
         return torch.sigmoid(x)
